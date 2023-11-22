4235ce7e2fa1148475916cbf7c4f48040b3f2df7
amanda laryssa <102298674+Amanda-Ward22@users.noreply.github.com>
2023-11-22 10:09:31 -0300

    Add files via upload
    exemplo de databse , so tem que ajustar

---
+++ aula.sql
@@ -0,0 +1,23 @@
+-- Tabela para armazenar informações dos usuários registrados por email
+CREATE TABLE UsuariosEmail (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    Nome VARCHAR(50),
+    Email VARCHAR(100) UNIQUE,
+    Senha VARCHAR(100) -- A senha seria criptografada na prática, mas por simplicidade será uma string aqui
+);
+    Origem VARCHAR(100)
+-- Tabela para armazenar informações dos usuários registrados pelo Google
+CREATE TABLE UsuariosGoogle (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    Nome VARCHAR(50),
+    Email VARCHAR(100) UNIQUE,
+    GoogleID VARCHAR(100) -- ID fornecido pelo Google para identificar o usuário
+);
+
+-- Tabela para armazenar informações dos usuários registrados pelo Facebook
+CREATE TABLE UsuariosFacebook (
+    ID INT PRIMARY KEY AUTO_INCREMENT,
+    Nome VARCHAR(50),
+    Email VARCHAR(100) UNIQUE,
+    FacebookID VARCHAR(100) -- ID fornecido pelo Facebook para identificar o usuário
+);
